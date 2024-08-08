5757215fa641b1c9feb3396ad17b52c59074c027
michael-le-forestier <MLeForestier@SINGULAR.CO.ZA>
2024-08-08 15:23:36 +0200

    Init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// src/App.tsx
+import React from 'react';
+import './App.css';
+import LandingPage from './LandingPage'; // Import the LandingPage component
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage />  {/* Render the LandingPage component */}
+    </div>
+  );
+}
+
+export default App;
